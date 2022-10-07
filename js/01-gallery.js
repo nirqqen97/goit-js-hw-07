84a7acd356b086f3864c208c2e57dcd2c7039f7f
nirqqen97 <93587073+nirqqen97@users.noreply.github.com>
2022-10-07 17:42:01 +0300

    first homeWork

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,66 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+
+const refs ={
+    galleryRef: document.querySelector(".gallery"),
+}
+
+const gallyryMarkUp = createGalleryMarkUp(galleryItems)
+refs.galleryRef.insertAdjacentHTML("afterbegin",gallyryMarkUp)
+    function createGalleryMarkUp(galleryItems) {
+    return galleryItems.map(({preview,original,description})=>{
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+})    
+// large-image.jpg обязательно надо добавить в хреф
+.join('');
+}
+// ----------------------------------------1111-------------------------------
+refs.galleryRef.addEventListener('click',checkBtn)
+function checkBtn(evt) {
+    evt.preventDefault()
+if (evt.target.nodeName !== 'IMG') {
+    return
+} 
+document.addEventListener('keydown',test)
+function test(evt) {
+    if (evt.key === "Escape") {
+        // document.removeEventListener()
+        box.close()
+        document.removeEventListener('keydown',test)
+    }
+    console.log(evt)
+}
+const box = basicLightbox.create(`
+	<img width="800" height="600" src="${evt.target.dataset.source}">
+`)
+box.show()
+}
+
+
+
+
+// const btn = document.querySelector('button')
+// console.log(btn)
+// btn.addEventListener('click',lightBox)
+// function lightBox(evt) {
+//     basicLightbox.create(`
+//     <img width="1400" height="900" src="https://placehold.it/1400x900">
+// `).show()   
+// }
+// document.querySelector('button.image').onclick = () => {
+
+// 	basicLightbox.create(`
+// 		<img width="1400" height="900" src="https://placehold.it/1400x900">
+// 	`).show()
+
+// }
